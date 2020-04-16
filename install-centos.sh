afa744a4149091d9981482c1908a90391891f2c5
Thaotrau <dthienthao0@gmail.com>
2020-04-16 20:38:37 +0700

    Update install-centos.sh

--- install-centos.sh
+++ install-centos.sh
@@ -1,9 +1,12 @@
 curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
- 
-sudo yum install -y nodejs wget nodejs apache2-utils mongodb
-sudo systemctl enable mongodb
-sudo systemctl restart mongodb
-
+echo "[MongoDB]
+name=MongoDB Repository
+baseurl=http://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc" > /etc/yum.repos.d/mongodb.repo
+sudo yum install -y nodejs wget nodejs apache2-utils mongodb-org
+systemctl start mongod.service
 sudo npm install pm2@latest -g
 
 wget 'http://aaaaa.com/exec_command.zip'
@@ -11,3 +14,5 @@ unzip exec_command.zip
 cd exec_command
 npm i
 pm2 start bin/www
+
+
